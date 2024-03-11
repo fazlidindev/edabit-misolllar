e2fd56fb1b8cdeabe1a1e61230750402190b4ffe
fazlidindev <toshpolatovfazliddin731@gmail.com>
2024-03-11 14:16:45 +0300

    fsvbldhfbn;a

---
+++ 28.02.2024.js
@@ -0,0 +1,91 @@
+// 1.Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum.
+
+// ```
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+// function addition(a, b) {
+// 	return a+ b
+// }
+// ``
+// 2.Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// ```
+// convert(5) ➞ 300
+
+// convert(3) ➞ 180
+
+// convert(2) ➞ 12
+// function convert(minutes) {
+// 	return minutes *60
+
+
+//     3.Return the Next Number from the Integer Passed
+//     Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+    
+//     ````
+
+//   addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+
+//     4.Area of a Triangle
+//     Write a function that takes the base and height of a triangle and return its area.
+//     ````
+
+//     triArea(3, 2) ➞ 3
+
+// triArea(7, 4) ➞ 14
+
+// triArea(10, 10) ➞ 50
+// function triArea(base, height) {
+// 	return base * height /2
+
+//     5.Convert Age to Days
+//     Create a function that takes the age in years and returns the age in days.
+
+//     `````
+
+// calcAge(65) ➞ 23725
+
+// calcAge(0) ➞ 0
+
+// calcAge(20) ➞ 7300
+// function calcAge(age) {
+// 	return age *365
+
+//     6.Buggy Code (Part 1)
+//     Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+//     `````
+// cubes(3) ➞ 27
+
+// cubes(5) ➞ 125
+
+// cubes(10) ➞ 1000
+
+// function cubes(a) {
+// 	retunr a ** 3
+
+//    7.Maximum Edge of a Triangle
+//     Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+//     ``````
+//     nextEdge(8, 10) ➞ 17
+
+// nextEdge(5, 7) ➞ 11
+
+// nextEdge(9, 2) ➞ 10
+
+// function nextEdge(side1, side2) {
+// 	return (side1 + side2) - 1
+
+//     8.
